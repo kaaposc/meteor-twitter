909c33e843fa003f0d5040915e84f80b28e6ab54
kaaposc <kaaposc@gmail.com>
2014-06-26 23:36:08 +0300

    Update package.js

--- package.js
+++ package.js
@@ -2,7 +2,7 @@ Package.describe({
   summary: "Twitter package for Meteor"
 });
 
-Npm.depends({node-twitter: "0.5.1"});
+Npm.depends({"node-twitter": "0.5.1"});
 
 Package.on_use(function (api) {
   api.add_files("main.js", "server");
