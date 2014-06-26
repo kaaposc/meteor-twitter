861a8ea9afa394b6835703cacbcf8aa70d5ae1f0
kaaposc <kaaposc@gmail.com>
2014-06-26 23:31:24 +0300

    Create package.js

---
+++ package.js
@@ -0,0 +1,12 @@
+Package.describe({
+  summary: "Twitter package for Meteor"
+});
+
+Npm.depends({node-twitter: "0.5.1"});
+
+Package.on_use(function (api) {
+  api.add_files("main.js", "server");
+  if(api.export) {
+    api.export('Twitter');
+  }
+});
